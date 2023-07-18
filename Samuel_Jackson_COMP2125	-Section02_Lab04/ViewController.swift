b80f4c1ecd38a1989f8414815c0b6211aebe05b7
Sam Jackson <100137493@student.georgianc.on.ca>
2023-07-18 12:58:50 -0400

    Initial Commit

---
+++ Samuel_Jackson_COMP2125	-Section02_Lab04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Samuel_Jackson_COMP2125	-Section02_Lab04
+//
+//  Created by user244653 on 7/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
